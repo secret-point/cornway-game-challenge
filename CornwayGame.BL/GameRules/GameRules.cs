331ed1103596b8b1e6780e8f12de1dc96d5bfd0b
alan-duarte <secret.point2020@gmail.com>
2024-08-30 00:10:18 -0400

    2.- Next Generation - Add Test for underpopulation Rule

--- CornwayGame.BL/GameRules/GameRules.cs
+++ CornwayGame.BL/GameRules/GameRules.cs
@@ -4,9 +4,45 @@ namespace CornwayGame.BL.GameRules
 {
     public class GameRules:IGameRules
     {
+        //Neighbors
+        //123
+        //4*5
+        //678
+
+        private readonly int[][] neighborCoordinates = new int[8][]{
+            new int[2] { -1, -1 },//position1
+            new int[2] { 0, -1 },//position2
+            new int[2] { 1, -1 },//position3
+        
+            new int[2] { -1, 0 },//position4
+            new int[2] { 1, 0 },//position5
+
+            new int[2] { -1, 1 },//position6
+            new int[2] { 0, 1 },//position7
+            new int[2] { 1, 1 },//position8
+        };
+
         public bool ShouldToggleCell(int i, int h, bool[][] board)
         {
-            return false;
+            var countLiveNeighbor = 0;
+            var countDeadNeighbor = 0;
+            for (int coordinateIndex = 0; coordinateIndex < neighborCoordinates.Length; coordinateIndex++)
+            {
+                int newI = i + neighborCoordinates[coordinateIndex][0];
+                int newH = h + neighborCoordinates[coordinateIndex][1];
+                if (newH < 0 || newI < 0 || newI >= board.Length || newH >= board[i].Length) continue;
+
+                if (board[newI][newH])
+                {
+                    countLiveNeighbor++;
+
+                }
+                else
+                {
+                    countDeadNeighbor++;
+                }
+            }
+            return countLiveNeighbor <2;
         }
     }
 }
